3e1b1a1104f455c501ff5a1c1ac27166e0eff521
Ololade Otemade <adeniyiotemade@gmail.com>
2023-01-24 01:06:49 +0100

    App set-up

---
+++ src/components/Meta.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Meta() {
+    return (
+        <Head>
+            <meta name="keywords" content="Real Estate, Rent, Management, For Sale" />
+            <meta name="description" content="Real Estate Website" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+            <title>Gram Stones</title>
+        </Head>
+    )
+}
